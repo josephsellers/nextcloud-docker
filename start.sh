213bff8248bab856d6a2052879f5da183d398e98
Joseph Sellers <6892567+josephsellers@users.noreply.github.com>
2023-08-31 10:10:22 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 GID=`stat -c "%g" /dev/dri/renderD128`
-groupadd -g $GID render2 || true # sometimes this is needed
+#groupadd -g $GID render2 || true # sometimes this is needed
 GROUP=`getent group $GID | cut -d: -f1`
 usermod -aG $GROUP www-data
 
